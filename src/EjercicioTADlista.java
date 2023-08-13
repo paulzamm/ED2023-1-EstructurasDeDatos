38ba6730886632d82fa6e15d474648fb1bd23c37
Paul Zambrano <altafuyapaul62@gmail.com>
2023-08-12 22:29:46 -0500

    ExamenP1 recuperacion

---
+++ src/EjercicioTADlista.java
@@ -0,0 +1,41 @@
+import algoritmos.Algoritmos;
+import algoritmos.AlgoritmosListas;
+import tadLista.Lista;
+import tadLista.TadLista;
+
+public class EjercicioTADlista {
+
+	public static void main(String[] args) {
+		TadLista<Integer> lista = new TadLista<>();
+		int resul, dato;
+		for(int i=0; i<=10; i++) {
+			AlgoritmosListas.insertarAlFinal(lista, Algoritmos.aleatorio(1, 20));
+		}
+		lista.imprimirLista();
+		dato = Algoritmos.aleatorio(1, 10);
+		resul = buscarEnLista(lista, dato);
+		if(resul != 0) {
+			System.out.println("Dato " + dato + " Encontrado en la pos: " + resul);
+		}else {
+			System.out.println("Dato " + dato + " No encontrado!");
+		}
+	}
+	
+	public static <T> int buscarEnLista(Lista<T> lista, T dato) {
+		Lista<T> aux = new TadLista<>();
+		aux.asignarReferencia(lista.devolverReferencia());
+		return buscarEnListaR(aux, dato, 1);
+	}
+	private static <T> int buscarEnListaR(Lista<T> lista, T dato, int cont) {
+		int resul=0;
+		if(!lista.esNulo()) {
+			if(lista.devolverClave() == dato) {
+				resul = cont;
+			}else {
+				lista.asignarReferencia(lista.devolverSiguiente());
+				resul = buscarEnListaR(lista, dato, cont+1);
+			}
+		}
+		return resul;
+	}
+}
